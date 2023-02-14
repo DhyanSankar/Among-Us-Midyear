df31543161cf2ee9fbb4b74ceb1053a6b270d1c7
Dhyan Sankar <dhyansankar007@gmail.com>
2023-02-13 22:59:50 -0500

    added transitioners for the levels

--- Assets/Scripts/NextLeveler.cs
+++ Assets/Scripts/NextLeveler.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NextLeveler : MonoBehaviour
+namespace UnityStandardAssets._2D
 {
-    // Start is called before the first frame update
-    void Start()
+    public class NextLeveler : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                String name = scene.name;
+                if(name == "Level 1")
+                    SceneManager.LoadScene("Level 2");
+                else if(name == "Level 2")
+                    SceneManager.LoadScene("Level 3");
+            }
+        }
     }
 }
